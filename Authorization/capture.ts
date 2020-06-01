bab98ddd5778fbfce542e118ab90ac994c28c636
Ishai Mika <ishai.mika@payfunc.com>
2020-06-01 18:25:04 +0200

    Added capture command

--- Authorization/capture.ts
+++ Authorization/capture.ts
@@ -1,7 +1,22 @@
 import * as gracely from "gracely"
 import * as cardfunc from "@cardfunc/model"
 import { Connection } from "../Connection"
+import { addCommand } from "./Module"
 
 export function capture(connection: Connection, authorization: string, amount?: number): Promise<cardfunc.Capture | gracely.Error> {
-	return connection.post<cardfunc.Capture>("private", `authorization/${ authorization }/capture`, amount ? amount : { amount })
+	return connection.post<cardfunc.Capture>("private", `authorization/${ authorization }/capture`, amount ? { amount } : {})
 }
+addCommand({
+	name: "capture",
+	description: "Adds a new capture.",
+	examples: [
+		["<authentication> <amount>", "Captures authorizations."],
+	],
+	execute: async (connection, argument, flags) => {
+		const amount = Number.parseFloat(argument[1])
+		const result = connection &&
+			await capture(connection, argument[0], amount)
+		console.info(JSON.stringify(result, undefined, "\t"))
+		return cardfunc.Capture.is(result)
+	}
+})
