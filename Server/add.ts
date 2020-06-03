b5e5e581989f7b15b3c0305137ecf14db4699a11
Ishai Mika <ishai@mika.se>
2020-06-03 14:48:15 +0200

    Tests all clearhaus errors. (#9)
    * Changed name on a variable and updated examples
    
    * Added more tests.
    
    * Added description and better names.
    
    Co-authored-by: Ishai Mika <ishai.mika@payfunc.com>

--- Server/add.ts
+++ Server/add.ts
@@ -3,18 +3,18 @@ import * as authly from "authly"
 import { Credentials } from "./Credentials"
 
 export async function add(name: string, privateKey: authly.Token, publicKey: authly.Token, user?: string, password?: string): Promise<boolean> {
-	const merchant: Credentials = { name, keys: { private: privateKey, public: publicKey } }
+	const credential: Credentials = { name, keys: { private: privateKey, public: publicKey } }
 	if (user && password)
-		merchant.administrator = { user, password }
-	return Credentials.save(merchant)
+		credential.administrator = { user, password }
+	return Credentials.save(credential)
 }
 
 addCommand({
 	name: "add",
-	description: "Adds a new merchant.",
+	description: "Adds a new server.",
 	examples: [
-		["<name> <private key> <public key>", "Add merchant without admin user."],
-		["<name> <private key> <public key> <user> <password>", "Add merchant and admin user."],
+		["<name> <private key> <public key>", "Add server without admin user."],
+		["<name> <private key> <public key> <user> <password>", "Add server and admin user."],
 	],
 	execute: (connection, argument, flags) => add(argument[0], argument[1], argument[2], argument[3], argument[4]),
 })
---
+++ Test/funds.ts
@@ -0,0 +1,26 @@
+import * as gracely from "gracely"
+import * as cardfunc from "@cardfunc/model"
+import * as Authorization from "../Authorization"
+import { addCommand } from "./Module"
+
+addCommand({
+	name: "funds",
+	description: "Insufficient funds (40413)",
+	examples: [],
+	execute: async (connection, argument, flags) => {
+		let result: boolean
+		const creatable: cardfunc.Authorization.Creatable = {
+			amount: 13.37,
+			currency: "SEK",
+			card: {
+				pan: "420000404130000",
+				expires: [ 2, 22 ],
+				csc: "987",
+			},
+		}
+		const token = connection && await Authorization.create(connection, creatable)
+		return gracely.client.malformedContent.is(token) &&
+			token.content.property == "card.pan" &&
+			token.content.description == "Invalid card number."
+	}
+})
