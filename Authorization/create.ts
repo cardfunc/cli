f4dd121e51156a33e7ec0a9d0e89cd60e2830317
Ishai Mika <ishai.mika@payfunc.com>
2020-06-03 14:27:52 +0200

    Added option to create authorization thru browser.

--- Authorization/create.ts
+++ Authorization/create.ts
@@ -1,3 +1,4 @@
+import * as open from "open"
 import * as isoly from "isoly"
 import * as cardfunc from "@cardfunc/model"
 import { addCommand } from "./Module"
@@ -7,15 +8,25 @@ import { Connection } from "../Connection"
 import { getPares, isParesError } from "./getPares"
 import { post } from "./post"
 
-export async function create(connection: Connection, authorization: cardfunc.Authorization.Creatable): Promise<authly.Token | gracely.Error> {
-	const error = await post(connection, authorization)
-	return isParesError(error) ? post(connection, { ...authorization, pares: (await getPares(error)).PaRes }) : error
+export async function create(connection: Connection, authorization: cardfunc.Authorization.Creatable, auto3d: boolean): Promise<authly.Token | gracely.Error> {
+	const response = await post(connection, authorization)
+	let result: authly.Token | gracely.Error
+	if (!isParesError(response))
+		result = response
+	else if (auto3d)
+		result = await post(connection, { ...authorization, pares: (await getPares(response)).PaRes })
+	else {
+		await open(`${ connection.url }/redirect/post?target=${ response.content.url }&PaReq=${ response.content.pareq }&TermUrl=https://postman-echo.com/post&MD=Kalle`)
+		result = response
+	}	
+	return result
 }
 addCommand({
 	name: "create",
 	description: "Creates a new authorization.",
 	examples: [
-		["13.37 EUR 4111111111111111 2/22 987", "Create an authorization for EUR 13.37."],
+		["13.37 EUR 4111111111111111 2/22 987", "Create an authorization for EUR 13.37 with 3D Secure in browser."],
+		["13.37 EUR 4111111111111111 2/22 987 auto", "Create an authorization for EUR 13.37."],
 		["13.37 EUR 4111111111111111 2/22 987 <pares>", "Create an authorization for EUR 13.37 with 3D Secure."]
 	],
 	execute: async (connection, argument, flags) => {
@@ -32,10 +43,10 @@ addCommand({
 					expires,
 					csc: argument[4],
 				},
-				pares: argument[5],
-				descriptor: argument[6]
-			})
-		console.info(typeof result == "string" ? result : JSON.stringify(result, undefined, "\t"))
+				pares: argument[5] == "auto" ? undefined : argument[5],
+				descriptor: argument[6],
+			}, argument[5] == "auto")
+		console.info("\n" + (typeof result == "string" ? result : JSON.stringify(result, undefined, "\t")))
 		return !!(typeof result == "string" && cardfunc.Authorization.verify(result))
 	}
 })
