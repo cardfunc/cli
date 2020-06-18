ecf897a9305d4803b8a2368c1856173b5d3f40f9
Ishai Mika <ishai.mika@payfunc.com>
2020-06-18 15:03:18 +0200

    Added card.

---
+++ Card/Module.ts
@@ -0,0 +1,14 @@
+import { Module } from "../Module"
+import { Command } from "../Command"
+
+const commands: { [command: string]: Command } = {}
+
+export function addCommand(command: Command) {
+	commands[command.name] = command
+}
+
+Module.register({
+	name: "card",
+	description: "Creates, updates and lists cards.",
+	commands,
+}, "card", "c")
