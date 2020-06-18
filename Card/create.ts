6364bf39c98190b000a6a0d4d415304df4544059
Ishai Mika <ishai.mika@payfunc.com>
2020-06-18 15:40:23 +0200

    Work in progress.

--- Card/create.ts
+++ Card/create.ts
@@ -11,12 +11,23 @@ addCommand({
 	name: "create",
 	description: "Create a new card.",
 	examples: [
-		["13.37 EUR 4111111111111111 2/22 987 <pares> <public key>", "Create a new card."],
+		["4111111111111111 2/22 987 <pares> <public key>", "Create a new card."],
 	],
 	execute: async (connection, argument, flags) => {
-		
-		const result = connection && cardfunc.Merchant.Creatable.is(merchant) && await create(connection, merchant)
-		console.info(JSON.stringify(result, undefined, "\t"))
-		return !gracely.Error.is(result)
+		const expires = argument[1].split("/", 2).map(e => Number.parseInt(e))
+		const result = connection && 
+			cardfunc.Card.Expires.is(expires) &&
+			await create(connection, {
+				card: {
+					pan: argument[0],
+					expires,
+					csc: argument[2],
+				},
+				pares: argument[3] == "auto" ? undefined : argument[3],
+			}, argument[5] == "auto")
+		console.info("\n" + (typeof result == "string" ? result : JSON.stringify(result, undefined, "\t")))
+		return !!(typeof result == "string" && cardfunc.Authorization.verify(result))
+		//console.info(JSON.stringify(result, undefined, "\t"))
+		//return !gracely.Error.is(result)
 	}
 })
