1abb6fda3946a3fb0a592e526db0c70f031e4f4e
Simon Mika <simon@mika.se>
2020-07-15 17:39:25 +0200

    Upgraded tests to use tokenized cards.

--- Test/cancel.ts
+++ Test/cancel.ts
@@ -1,6 +1,8 @@
 import * as gracely from "gracely"
+import * as authly from "authly"
 import * as cardfunc from "@cardfunc/model"
 import * as Authorization from "../Authorization"
+import * as Card from "../Card"
 import { addCommand } from "./Module"
 
 addCommand({
@@ -8,21 +10,22 @@ addCommand({
 	description: "Creates and cancels an authorization.",
 	examples: [],
 	execute: async (connection, argument, flags) => {
-		let result: boolean
-		const creatable: cardfunc.Authorization.Creatable = {
+		const card = connection && await Card.create(connection, {
+			pan: "4111111111111111",
+			expires: [ 2, 22 ],
+			csc: "987",
+		})
+		const creatable = authly.Token.is(card) && {
 			amount: 13.37,
 			currency: "SEK",
-			card: {
-				pan: "4111111111111111",
-				expires: [ 2, 22 ],
-				csc: "987",
-			},
+			card,
 		}
-		const token = connection && await Authorization.create(connection, creatable, true)
+		const token = connection && cardfunc.Authorization.Creatable.is(creatable) && await Authorization.create(connection, creatable, true)
+		let result: boolean
 		if (result = !gracely.Error.is(token)) {
 			const a = token && await cardfunc.Authorization.verify(token) || undefined
 			const c = connection && token && await Authorization.cancel(connection, token)
-			result = cardfunc.Authorization.is(a) && a.amount == creatable.amount && cardfunc.Cancel.is(c)
+			result = cardfunc.Authorization.Creatable.is(creatable) && cardfunc.Authorization.is(a) && a.amount == creatable.amount && cardfunc.Cancel.is(c)
 		}
 		return result
 	}
--- Test/standard.ts
+++ Test/standard.ts
@@ -1,6 +1,7 @@
-import * as gracely from "gracely"
+import * as authly from "authly"
 import * as cardfunc from "@cardfunc/model"
 import * as Authorization from "../Authorization"
+import * as Card from "../Card"
 import { addCommand } from "./Module"
 
 addCommand({
@@ -8,22 +9,23 @@ addCommand({
 	description: "Creates, captures and refunds an authorization.",
 	examples: [],
 	execute: async (connection, argument, flags) => {
-		let result: boolean
-		const creatable: cardfunc.Authorization.Creatable = {
+		const card = connection && await Card.create(connection, {
+			pan: "4111111111111111",
+			expires: [ 2, 22 ],
+			csc: "987",
+		})
+		const creatable = authly.Token.is(card) && {
 			amount: 13.37,
 			currency: "SEK",
-			card: {
-				pan: "4111111111111111",
-				expires: [ 2, 22 ],
-				csc: "987",
-			},
+			card,
 		}
-		const token = connection && await Authorization.create(connection, creatable, true)
-		if (result = !gracely.Error.is(token)) {
-			const a = token && await cardfunc.Authorization.verify(token) || undefined
+		const token = connection && cardfunc.Authorization.Creatable.is(creatable) && await Authorization.create(connection, creatable, true)
+		let result: boolean
+		if (result = authly.Token.is(token)) {
+			const a = await cardfunc.Authorization.verify(token) || undefined
 			const c = connection && token && await Authorization.capture(connection, token)
 			const r = connection && token && await Authorization.refund(connection, token)
-			result = cardfunc.Authorization.is(a) && cardfunc.Capture.is(c) && cardfunc.Refund.is(r) && creatable.amount == a.amount && creatable.amount == c.amount && creatable.amount == r.amount
+			result = cardfunc.Authorization.Creatable.is(creatable) && cardfunc.Authorization.is(a) && cardfunc.Capture.is(c) && cardfunc.Refund.is(r) && creatable.amount == a.amount && creatable.amount == c.amount && creatable.amount == r.amount
 		}
 		return result
 	}
