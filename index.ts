0b093b39457ada731357dd0869644462528d3d8b
Simon Mika <simon@mika.se>
2020-07-20 14:49:18 +0200

    Added export of Connection.

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as Authorization from "./Authorization"
 import * as Card from "./Card"
+import { Connection } from "./Connection"
 import * as Server from "./Server"
 import * as Test from "./Test"
 
 export {
 	Authorization,
 	Card,
+	Connection,
 	Server,
 	Test,
 }
