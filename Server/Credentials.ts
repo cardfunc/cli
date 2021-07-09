f3af6215fcd9dcb6e891ff4923b5f88c9acb9b17
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2021-07-09 10:58:52 +0200

    Added ability to add multiple env configurations (#59)
    * Added ability to add multiple env configurations
    
    * Reworked slightly

--- Server/Credentials.ts
+++ Server/Credentials.ts
@@ -2,6 +2,13 @@ import * as authly from "authly"
 
 export interface Credentials {
 	name: string
-	keys: { private: authly.Token; public: authly.Token; agent: authly.Token }
-	administrator?: { user: string; password: string }
+	keys: {
+		private: authly.Token | undefined
+		public: authly.Token | undefined
+		agent: authly.Token | undefined
+	}
+	administrator?: {
+		user: string | undefined
+		password: string | undefined
+	}
 }
--- Server/Storage.ts
+++ Server/Storage.ts
@@ -14,17 +14,14 @@ export class Storage {
 		return !!((await this.initialized) && (await this.backend.setItem(merchant.name, merchant))).file
 	}
 	async load(name: string): Promise<Credentials | undefined> {
-		return name == "env"
-			? {
-					name: "env",
-					keys: {
-						private: process.env.privateKey ?? undefined,
-						public: process.env.publicKey ?? undefined,
-						agent: process.env.agentKey ?? undefined,
-					},
-					administrator: { user: process.env.adminUser ?? undefined, password: process.env.adminPassword ?? undefined },
-			  }
-			: (await this.initialized) && this.backend.getItem(name)
+		const envName: string = name == "env" ? "" : name
+		const envCapitalized: string | undefined =
+			envName.length > 0 ? envName.substring(0, 1).toUpperCase() + envName.substring(1, envName.length) : undefined
+		const result =
+			this.checkEnv(name, envName) ??
+			this.checkEnv(name, envCapitalized) ??
+			((await this.initialized) && this.backend.getItem(name))
+		return result
 	}
 	async list(): Promise<string[]> {
 		return (await this.initialized) && this.backend.keys()
@@ -32,6 +29,22 @@ export class Storage {
 	async remove(name: string): Promise<boolean> {
 		const result = (await this.initialized) && (await this.backend.removeItem(name))
 		return result.removed
+	}
+	private checkEnv(name: string, envName: string | undefined): Credentials | undefined {
+		return envName != undefined && process.env["privateKey" + envName] && process.env["publicKey" + envName]
+			? {
+					name,
+					keys: {
+						private: process.env["privateKey" + envName] ?? undefined,
+						public: process.env["publicKey" + envName] ?? undefined,
+						agent: process.env["agentKey" + envName] ?? undefined,
+					},
+					administrator: {
+						user: process.env["adminUser" + envName] ?? undefined,
+						password: process.env["adminPassword" + envName] ?? undefined,
+					},
+			  }
+			: undefined
 	}
 	private static opened: { [name: string]: Storage } = {}
 	static open(name: string): Storage {
